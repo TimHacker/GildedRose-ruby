bb44339eea0dd506d1a5ad15689fb1e594674c33
Yasmin Morshed <yasmin.morshed@madetech.com>
2023-07-03 17:15:39 +0100

    Add a test, change the days

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -6,8 +6,15 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
     end
-  end
+
+    it "decreases sell-in and quality by 1" do
+      items = [Item.new("bar", 1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 0
+      expect(items[0].quality).to eq 0
+    end
+  end  
 
 end
