afe11e3b9adab0b8c52730b3510d299c91b70afc
Yasmin Morshed <yasmin.morshed@madetech.com>
2023-07-24 16:54:51 +0100

    Refactor to use update item method

--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -2,46 +2,47 @@
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
+def update_item(name, sell_in, quality, occurrences = 1)
+  items = [Item.new(name, sell_in, quality)]
+  occurrences.times do
+    GildedRose.new(items).update_quality
+  end
+  items
+end
+
 describe GildedRose do
   describe '#update_quality' do
     it 'does not change the name' do
-      items = [Item.new('foo', 0, 0)]
-      GildedRose.new(items).update_quality
+      items = update_item('foo', 0, 0)
       expect(items[0].name).to eq 'foo'
     end
 
     it 'decreases sell-in and quality by 1 for normal item' do
-      items = [Item.new('bar', 1, 1)]
-      GildedRose.new(items).update_quality
+      items = update_item('bar', 1, 1)
       expect(items[0].sell_in).to eq 0
       expect(items[0].quality).to eq 0
     end
 
     it 'decreases quality twice as fast once the item is past its sell-in date' do
-      items = [Item.new('soap', -1, 2)]
-      GildedRose.new(items).update_quality
+      items = update_item('soap', -1, 2)
       expect(items[0].sell_in).to eq(-2)
       expect(items[0].quality).to eq 0
     end
 
     it 'quality of an item is never negative' do
-      items = [Item.new('soap', 3, 0)]
-      GildedRose.new(items).update_quality
-      GildedRose.new(items).update_quality
+      items = update_item('soap', 3, 0, 2)
       expect(items[0].sell_in).to eq 1
       expect(items[0].quality).to eq 0
     end
 
     it 'aged brie increases in quality with time' do
-      items = [Item.new('Aged Brie', 3, 49)]
-      GildedRose.new(items).update_quality
+      items = update_item('Aged Brie', 3, 49)
       expect(items[0].sell_in).to eq 2
       expect(items[0].quality).to eq 50
     end
 
     it 'the quality of an item can never be over 50' do
-      items = [Item.new('Aged Brie', 3, 50)]
-      GildedRose.new(items).update_quality
+      items = update_item('Aged Brie', 3, 50)
       expect(items[0].sell_in).to eq 2
       expect(items[0].quality).to eq 50
     end
